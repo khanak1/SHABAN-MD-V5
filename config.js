d4de8af64ce74c0a7ee8de6a32a3d2189b3cb0b8
khanak1 <144924515+khanak1@users.noreply.github.com>
2025-07-01 12:46:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group  
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/j9G5tmNM/shaban-md.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
 // add custom menu and mention reply image url   
 BOT_NAME: process.env.BOT_NAME || "Silent Lover",
 // add bot namw here for menu
@@ -40,7 +40,7 @@ OWNER_NAME: process.env.OWNER_NAME || "AFZAAL KHAN",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© Powered By Khan*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/tasodv.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/2a06381b260c3f096a612.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *KHAN*⚡",
 // add alive msg here 
